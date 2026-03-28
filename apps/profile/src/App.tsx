78613608bcb8fe67e116671075f3be24732141ae
serj.baturin <sergei.baturin+gl@shelterzoom.com>
2026-03-28 00:06:51 -0300

    Routing 404 checking

--- apps/profile/src/App.tsx
+++ apps/profile/src/App.tsx
@@ -1,25 +1,43 @@
 import { AppThemeProvider, Button } from "@repo/core";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { BrowserRouter, Link } from "react-router-dom";
+import { RouterConfig } from "./router";
+
+function Nav() {
+
+    return (
+        <Box
+            component="nav"
+            sx={{
+                display: "flex",
+                gap: 2,
+                px: 4,
+                py: 2,
+                borderBottom: 1,
+                borderColor: "divider",
+                alignItems: "center",
+            }}
+        >
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                Admin
+            </Typography>
+            <Link to="/">Home</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/settings">Settings</Link>
+
+                <Button>Sign in</Button>
+        </Box>
+    );
+}
 
 export function App() {
-  return (
-    <AppThemeProvider>
-      <Box
-        sx={{
-          minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: 3,
-        }}
-      >
-        <Typography variant="h3" component="h1">
-          Profile
-        </Typography>
-        <Button>Get Started</Button>
-      </Box>
-    </AppThemeProvider>
-  );
+    return (
+        <BrowserRouter>
+            <AppThemeProvider>
+                <Nav />
+                <RouterConfig />
+            </AppThemeProvider>
+        </BrowserRouter>
+    );
 }
