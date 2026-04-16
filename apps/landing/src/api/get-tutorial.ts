d748eaee687a8a9c4a64c57eb11d4367421a9c7c
serj.baturin <sergei.baturin+gl@shelterzoom.com>
2026-04-16 17:33:25 -0300

    Fix get-tutorial page bug on prod: TRY # 2

--- apps/landing/src/api/get-tutorial.ts
+++ apps/landing/src/api/get-tutorial.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL
+// const API_URL = import.meta.env.VITE_API_URL
 
 export interface GetTutorialSuccess {
   success: true
@@ -14,11 +14,15 @@ export interface GetTutorialError {
 export type GetTutorialResponse = GetTutorialSuccess | GetTutorialError
 
 export const postGetTutorial = async (email: string, pdf: string): Promise<GetTutorialResponse> => {
-  const response = await fetch(`${API_URL}/get-tutorial`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email: email, pdf: pdf }),
-  })
+  // const response = await fetch(`${API_URL}/get-tutorial`, {
+  const response = await fetch(
+    `https://luravoqnf8.execute-api.us-west-1.amazonaws.com/v1/get-tutorial`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: email, pdf: pdf }),
+    }
+  )
 
   return (await response.json()) as GetTutorialResponse
 }
